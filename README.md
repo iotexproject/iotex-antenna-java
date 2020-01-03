4853c4c3de372d9125658013411f25e907fdff24
ququzone <xueping.yang@gmail.com>
2020-01-03 13:12:08 +0800

    change base64

--- README.md
+++ README.md
@@ -14,14 +14,14 @@ network for IoT powered by scalability- and privacy-centric blockchains. Please 
 <dependency>
   <groupId>com.github.iotexproject</groupId>
   <artifactId>iotex-antenna-java</artifactId>
-  <version>0.5.3</version>
+  <version>0.5.4</version>
 </dependency>
 ```
 
 ### Install by Gradle
 
 ```
-implementation 'com.github.iotexproject:iotex-antenna-java:0.5.3'
+implementation 'com.github.iotexproject:iotex-antenna-java:0.5.4'
 ```
 
 
--- src/main/java/com/github/iotexproject/antenna/jwt/EK256K.java
+++ src/main/java/com/github/iotexproject/antenna/jwt/EK256K.java
@@ -5,7 +5,7 @@ import com.github.iotexproject.antenna.account.IotexAccount;
 import com.github.iotexproject.antenna.utils.Numeric;
 import com.google.common.base.Charsets;
 import com.google.gson.Gson;
-import org.apache.commons.codec.binary.Base64;
+import com.lambdaworks.codec.Base64;
 
 import java.util.Map;
 
@@ -16,9 +16,9 @@ public class EK256K {
     public static String sign(String payloadJson, String privateKey) {
         Account account = IotexAccount.create(privateKey);
 
-        String headerEnc = base64URLDecode(header.getBytes(Charsets.UTF_8));
-        String payloadEnc = base64URLDecode(payloadJson.getBytes(Charsets.UTF_8));
-        String signature = base64URLDecode(account.sign((headerEnc + "." + payloadEnc).getBytes(Charsets.UTF_8)));
+        String headerEnc = base64URLEncode(header.getBytes(Charsets.UTF_8));
+        String payloadEnc = base64URLEncode(payloadJson.getBytes(Charsets.UTF_8));
+        String signature = base64URLEncode(account.sign((headerEnc + "." + payloadEnc).getBytes(Charsets.UTF_8)));
 
         return headerEnc + "." + payloadEnc + "." + signature;
     }
@@ -30,13 +30,13 @@ public class EK256K {
             throw new RuntimeException("token invalid");
         }
 
-        String header = new String(Base64.decodeBase64(segments[0]));
+        String header = new String(base64URLDecode(segments[0]));
         if (!header.contains("\"alg\":\"EK256K\"")) {
             throw new RuntimeException("alg should be EK256K");
         }
 
-        String payload = new String(Base64.decodeBase64(segments[1]));
-        byte[] signature = Base64.decodeBase64(segments[2]);
+        String payload = new String(base64URLDecode(segments[1]));
+        byte[] signature = base64URLDecode(segments[2]);
 
         IotexAccount empty = (IotexAccount) IotexAccount.create("");
         String recoveredAddress = empty.recover(signature, (segments[0] + "." + segments[1]).getBytes());
@@ -50,8 +50,19 @@ public class EK256K {
         return payload;
     }
 
-    private static String base64URLDecode(byte[] data) {
-        return new String(Base64.encodeBase64URLSafe(data));
+    private static String base64URLEncode(byte[] data) {
+        String result = new String(Base64.encode(data));
+        return result
+                .replaceAll("\\+", "-")
+                .replaceAll("/", "_")
+                .replaceAll("=", "");
+    }
+
+    private static byte[] base64URLDecode(String data) {
+        String result = data
+                .replaceAll("-", "+")
+                .replaceAll("_", "/");
+        return Base64.decode(result.toCharArray());
     }
 
     public static Object extractFromPayload(String payloadJson, String key) {
